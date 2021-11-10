3083fb298072314287ad7f8b1132e51a1324a7e3
Opilane TTHK <opilane@tthk.ee>
2021-11-10 10:38:23 +0200

    ConsoleUI

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"E:\Temp\Demos\FileSystem";
+
+            //string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+
+            //foreach (string dir in dirs)
+            //{
+            //    Console.WriteLine(dir);
+            //}
+
+            var files = Directory.GetFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly);
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
